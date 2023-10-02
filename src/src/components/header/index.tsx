76a6edc42f13c77590101ef6d8bdcf972de5387f
Weverson-Luan <wltech2022@gmail.com>
2023-10-02 20:57:50 -0300

    setup: :rocket: setup do projeto

---
+++ src/src/components/header/index.tsx
@@ -0,0 +1,17 @@
+/**
+ * IMPORTS
+ */
+import "./styles.css";
+
+const Header = () => {
+  return (
+    <header className="header-container">
+      <p>ola sou header</p>
+    </header>
+  );
+};
+
+/**
+ * EXPORTS
+ */
+export { Header };
